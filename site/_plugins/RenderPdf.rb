2b8947051acf7b9e10e278f3104c1686a94a4236
Rowan <dragonologist.ro@gmail.com>
2024-09-18 01:08:57 -0400

    Finally add resume page & resume

---
+++ site/_plugins/RenderPdf.rb
@@ -0,0 +1,23 @@
+module Jekyll
+    class PdfTag < Liquid::Tag
+        def initialize(tag_name, pathname, tokens)
+        @pathname = pathname
+        end
+        
+        def render(context)
+        output = <<~HTML
+                    <object type="application/pdf" data="#{@pathname}#toolbar=0&navpanes=0" width="100%" height="900px">
+                        PDF viewer not working. Click 
+                        <a href="#{@pathname}">here</a> to download.
+                    </object>
+                    HTML
+        output
+        end
+    
+        def parse_content(content)
+        end
+    
+    end
+    end
+
+    Liquid::Template.register_tag('render_pdf', Jekyll::PdfTag)
---
+++ site/secondary/resume.md
@@ -0,0 +1,12 @@
+---
+layout: secondary
+title: Resume
+permalink: /resume/
+---
+
+Updated September 18, 2024.
+
+For contact, you can reach me by email at
+`firstname at lastname dot 🇨🇦`
+
+{% render_pdf ../assets/pdfs/resume.pdf %}
